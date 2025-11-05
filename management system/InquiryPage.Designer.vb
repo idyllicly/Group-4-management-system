d14f15b4a60f4bfb3ecd99a3576730de1ea1ba48
lbrit <lbrit@NOIR>
2025-11-05 21:08:13 +0800

    added inquiry form1

--- management system/InquiryPage.Designer.vb
+++ management system/InquiryPage.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class homepage
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InquiryPage
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()>
+    <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,47 +20,32 @@ Partial Class homepage
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()>
+    <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.UserControl11 = New management_system.UserControl1()
-        Me.UserControl12 = New management_system.UserControl1()
         Me.SuspendLayout()
         '
         'UserControl11
         '
-        Me.UserControl11.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.UserControl11.BackColor = System.Drawing.Color.White
-        Me.UserControl11.Location = New System.Drawing.Point(-3, -2)
+        Me.UserControl11.Location = New System.Drawing.Point(1, 1)
         Me.UserControl11.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.UserControl11.Name = "UserControl11"
-        Me.UserControl11.Size = New System.Drawing.Size(1625, 86)
+        Me.UserControl11.Size = New System.Drawing.Size(1632, 74)
         Me.UserControl11.TabIndex = 0
         '
-        'UserControl12
-        '
-        Me.UserControl12.BackColor = System.Drawing.Color.White
-        Me.UserControl12.Location = New System.Drawing.Point(52, 51)
-        Me.UserControl12.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.UserControl12.Name = "UserControl12"
-        Me.UserControl12.Size = New System.Drawing.Size(8, 17)
-        Me.UserControl12.TabIndex = 1
-        '
-        'homepage
+        'InquiryPage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1626, 684)
-        Me.Controls.Add(Me.UserControl12)
+        Me.ClientSize = New System.Drawing.Size(1191, 651)
         Me.Controls.Add(Me.UserControl11)
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.Name = "homepage"
-        Me.Text = "homepage"
+        Me.Name = "InquiryPage"
+        Me.Text = "Form1"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents UserControl11 As UserControl1
-    Friend WithEvents UserControl12 As UserControl1
 End Class
--- management system/UserControl1.Designer.vb
+++ management system/UserControl1.Designer.vb
@@ -32,43 +32,46 @@ Partial Class UserControl1
         '
         Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Button1.Location = New System.Drawing.Point(1735, 24)
+        Me.Button1.Location = New System.Drawing.Point(1542, 19)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(82, 48)
+        Me.Button1.Size = New System.Drawing.Size(73, 38)
         Me.Button1.TabIndex = 0
         Me.Button1.UseVisualStyleBackColor = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("ROG Fonts", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label1.Location = New System.Drawing.Point(143, 24)
+        Me.Label1.Location = New System.Drawing.Point(127, 19)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(547, 48)
+        Me.Label1.Size = New System.Drawing.Size(304, 39)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "RRC Pest Control"
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.management_system.My.Resources.Resources.logo
-        Me.PictureBox1.Location = New System.Drawing.Point(23, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(20, 10)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(105, 72)
+        Me.PictureBox1.Size = New System.Drawing.Size(93, 58)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
         '
         'UserControl1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "UserControl1"
-        Me.Size = New System.Drawing.Size(1836, 93)
+        Me.Size = New System.Drawing.Size(1632, 74)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
